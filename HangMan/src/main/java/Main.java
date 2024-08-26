3ebaa61b3f7c67c306e8581877139514b980e6e6
202100914 <202100914@estudantes.ips.pt>
2024-08-26 17:56:00 +0100

    Nível 2
    Concluído!

--- HangMan/src/main/java/Main.java
+++ HangMan/src/main/java/Main.java
@@ -2,13 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        InputReader inputReader = new InputReader();
+        WordGuessingGame word = new WordGuessingGame();
 
-        // Teste básico com uma pergunta
-        char input = inputReader.getChar("Digite uma letra: ");
-
-        // Imprime o caractere que foi digitado
-        System.out.println("Você digitou: " + input);
+        word.play();
 
         // Encerrando o programa
         System.exit(0);
--- HangMan/src/main/java/WordGuessingGame.java
+++ HangMan/src/main/java/WordGuessingGame.java
@@ -2,11 +2,13 @@ public class WordGuessingGame {
     private String hiddenWord;
     private String guessedWord;
     private static int numberOfTries;
+    private InputReader reader;
 
     public WordGuessingGame(){
         this.hiddenWord = "abc";
         this.guessedWord = "___";
         this.numberOfTries = 0;
+        reader = new InputReader();
     }
 
     public String getHiddenWord() {
@@ -21,7 +23,55 @@ public class WordGuessingGame {
         return numberOfTries;
     }
 
-    public void showGuessedWord(){
+    public void play(){
+        showWelcome();
+
+        do{
+            char letter = reader.getChar("");
+
+            guess(letter);
+
+            showGuessedWord();
+            numberOfTries++;
+        }while (!guessedWord.equals(hiddenWord));
+
+        System.out.println("Parabéns! Você adivinhou a palavra " + hiddenWord);
+        System.out.println("Numero de tentativas: " + getNumberOfTries());
+    }
+
+    private void showWelcome(){
+        String titulo = "*********************************";
+        String mensagem = "    BEM-VINDO AO JOGO DA FORCA";
+        String instrucoes = "  Tente adivinhar a palavra " + guessedWord + " letra por letra.";
+        String mensagemFinal = "    Boa sorte e divirta-se!";
+
+        System.out.println(titulo);
+        System.out.println(mensagem);
+        System.out.println(instrucoes);
+        System.out.println(mensagemFinal);
+        System.out.println(titulo);
+    }
+
+    private void guess(char letter){
+        boolean isCorrect = false;
+        StringBuilder guessedWord = new StringBuilder(this.guessedWord);
+
+        for(int i = 0; i < this.hiddenWord.length(); i++){
+            if (this.hiddenWord.charAt(i) == letter){
+                isCorrect = true;
+                guessedWord.setCharAt(i, letter);
+            }
+        }
+
+        if(isCorrect){
+            this.guessedWord = guessedWord.toString();
+            System.out.print("Acertou! ");
+        } else {
+            System.out.print("Errou! ");
+        }
+    }
+
+    private void showGuessedWord(){
         System.out.println(getGuessedWord());
     }
 }
